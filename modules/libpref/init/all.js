ac3c53bc0c9ef7c50cfd966284f8a6285b9240bb
Rob Wu <rob@robwu.nl>
2026-02-11 19:44:54 +0000

    Bug 1799344 - Remove user gesture restriction from action.openPopup r=rpl
    Differential Revision: https://phabricator.services.mozilla.com/D280762

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -1793,11 +1793,7 @@ pref("extensions.manifestV2.actionsPopupURLRestricted", false);
   pref("extensions.backgroundServiceWorker.enabled", false, locked);
 #endif
 // Whether to enable the updated openPopup API.
-#ifdef NIGHTLY_BUILD
-  pref("extensions.openPopupWithoutUserGesture.enabled", true);
-#else
-  pref("extensions.openPopupWithoutUserGesture.enabled", false);
-#endif
+pref("extensions.openPopupWithoutUserGesture.enabled", true);
 // Install origins restriction.
 pref("extensions.install_origins.enabled", false);
 
